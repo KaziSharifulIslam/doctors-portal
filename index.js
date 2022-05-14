1a4395aa669e645d08a954148eb30f13f801eae5
Kazi Shariful Islam <kazisharifulislam52@gmail.com>
2022-05-14 21:55:23 +0600

    none

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ async function run() {
 }
 run().catch(console.dir);
 app.get("/", (req, res) => {
-  res.send("Hello Heroku!! heoku new server");
+  res.send("Hello Heroku!! heoku new server dosn't work ");
 });
 app.listen(port, () => console.log(`listening at http://localhost:${port}`));
