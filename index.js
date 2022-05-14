17747d8f6c6a8077f68a01a5a017816445d09f30
Kazi Shariful Islam <kazisharifulislam52@gmail.com>
2022-05-14 19:44:25 +0600

    ready for heroku

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+const res = require("express/lib/response");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+// MONGODB CONNECT
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h8cst.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri);
+console.log(uri);
+async function run() {
+  try {
+    await client.connect();
+    console.log(`db connected`);
+    const serviceCollection = client.db("doctors_portal").collection("services");
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+      // console.log(result);
+    });
+  } finally {
+    // client.close();
+  }
+}
+run().catch(console.dir);
+app.get("/", (req, res) => {
+  res.send("Hey, Bangladeshi Mango people!");
+});
+app.listen(port, () => console.log(`listening at http://localhost:${port}`));
